d60dd372944750e19c66add83e52d0dabb8ae86f
Salvamar22 <62631098+Salvamar22@users.noreply.github.com>
2021-03-22 11:36:03 -0600

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{   //Declaración de variables
+
+    int number; //Variable Numero
+    string word;//Variable Palabra
+    char h;//Variable Caracter
+    double numbers;//Variable decimales
+    float n;//Variable flotante(decimales)
+
+    //Pidiendo datos al usuario
+    cout<<"Digite un numero"<<endl;
+    cin>>number;
+    cout<<"Digite una palabra"<<endl;
+    cin>>word;
+    cout<<"Digite un caracter. Ejemplo'H'"<<endl;
+    cin>>h;
+    cout<<"Digite un numero decimal"<<endl;
+    cin>>numbers;
+    cout<<"Digite un numero decimal"<<endl;
+    cin>>n;
+    //Impriendo datos ingresados por el usuario
+    cout<<"Mi numero es: "<<number<<endl;
+    
+    cout<<"Mi palabra es: "<<word<<endl;
+    
+    cout<<"Mi caracter es: "<<h<<endl;
+    
+    cout<<"Mi numero es: "<<numbers<<endl;
+    
+    cout<<"Mi numero es: "<<n<<endl;
+
+    return 0;
+}
